420b7550ad86fe65090e11bb2c3568833de43e92
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-09-11 16:36:11 +0200

    Initial commit

---
+++ scripts/check_python_dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+working_dir=$(pwd)
+project_dir=$(dirname $(dirname $(realpath $0)))
+
+cd $project_dir
+
+# backup
+cp requirements.txt requirements.txt.tmp
+
+uv pip compile -q --universal -p 3.11 --annotation-style=line \
+    --extra=plugins \
+    --output-file=requirements.txt \
+    pyproject.toml
+
+diff requirements.txt.tmp requirements.txt
+ 
+# cleanup
+mv requirements.txt.tmp requirements.txt
